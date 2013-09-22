b94fb655fd62f5a1ae64207f369537977457dcd6
MakerSquare Student <students@themakersquare.com>
2013-09-21 22:41:54 -0500

    adding model for videos and seed file

--- db/schema.rb
+++ db/schema.rb
@@ -11,6 +11,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 20130922032851) do
+
+  create_table "videos", force: true do |t|
+    t.string   "title"
+    t.string   "youtube_id"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,36 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+
+
+
+
+Video.destroy_all
+
+videos = [
+	{
+		title: "America is not the greatest country",
+		youtube_id: "q49NOyJ8fNA"
+	},
+	{
+		title: "Gladiator 'Are You Not Entertained' Scene",
+		youtube_id: "FI1ylg4GKv8"
+	},
+	{
+		title: "Spirit Fingers",
+		youtube_id: "Nj-F4OvzyLM"
+	},
+	{
+		title: "Toy Story: Sid Learns a Lesson",
+		youtube_id: "gsusakRf7T8"
+	},
+	{
+		title: "Brownie Scene from Matilda",
+		youtube_id: "EVWOQwZENBg"
+	}
+]
+
+videos.each do |video|
+	Video.create(title: video[:title], youtube_id: video[:youtube_id])
+end
